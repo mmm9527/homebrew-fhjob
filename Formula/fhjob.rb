313def4efe0bdeaf1efe98ed4e10473a5dd5282b
wuhen <28866138@qq.com>
2021-03-23 17:24:42 +0800

    v0.2.0

--- Formula/fhjob.rb
+++ Formula/fhjob.rb
@@ -4,9 +4,9 @@
 class Fhjob < Formula
     desc "A commandline tool for easily deploy fuhsi's analysis job"
     homepage "https://github.com/jy00464346/fhjob"
-    url "https://github.com/jy00464346/fhjob/releases/download/v0.1.0/fhjob-darwin.tar.gz"
-    sha256 "ae70dda6c76a714e3068fc6f0b92fd438dd5c7f37a66ee531641cfb18c4e859b"
-    version "0.1.0"
+    url "https://github.com/jy00464346/fhjob/releases/download/v0.2.0/fhjob-darwin.tar.gz"
+    sha256 "fff201a334a08bfd1e75e0d0ec493a9bac282ddad6b9ab97895c2539df769bd6"
+    version "0.2.0"
 
     def install
       bin.install "fhjob"
